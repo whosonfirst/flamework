44c4d151982db2eac61031b48973713c66dcd3dc
whosonfirst@mapzen.com <whosonfirst@mapzen.com>
2017-07-14 17:08:26 +0000

    stub out hooks to use libsodium

--- www/include/lib_crypto.php
+++ www/include/lib_crypto.php
@@ -1,4 +1,13 @@
 <?php
+
+	# https://paragonie.com/blog/2015/05/using-encryption-and-authentication-correctly
+	# https://paragonie.com/blog/2015/05/if-you-re-typing-word-mcrypt-into-your-code-you-re-doing-it-wrong
+	# https://paragonie.com/blog/2017/02/cryptographically-secure-php-development
+	
+	# https://paragonie.com/blog/2017/06/libsodium-quick-reference-quick-comparison-similar-functions-and-which-one-use
+	# https://paragonie.com/book/pecl-libsodium/read/09-recipes.md#encrypted-cookies
+	# https://github.com/defuse/php-encryption/blob/master/docs/Tutorial.md
+	
 	if (!defined('MCRYPT_RIJNDAEL_256')){
 		 die("[lib_crypto] Flamework requires MCRYPT_RIJNDAEL_256");
 	}
